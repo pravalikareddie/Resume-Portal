34376dd12f5d624647666f2c639e1404936b3e2e
Pravalika Kanchala <pravalikakanchala11@gmail.com>
2020-10-21 21:15:29 -0400

    setup table relations

--- src/main/java/com/pravalika/projects/resumeportal/HomeController.java
+++ src/main/java/com/pravalika/projects/resumeportal/HomeController.java
@@ -1,5 +1,6 @@
 package com.pravalika.projects.resumeportal;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pravalika.projects.resumeportal.models.Job;
 import com.pravalika.projects.resumeportal.models.User;
 import com.pravalika.projects.resumeportal.models.UserProfile;
 import com.pravalika.projects.resumeportal.repository.UserProfileRepository;
@@ -19,7 +21,29 @@ public class HomeController {
 	private UserProfileRepository userProfileRepository;
 	@GetMapping("/home")
 	public String test() {
-		return "Hello User";
+		   Optional<UserProfile> profileOptional = userProfileRepository.findByUserName("einstein");
+	        profileOptional.orElseThrow(() -> new RuntimeException("Not found "));
+
+	        UserProfile profile1 = profileOptional.get();
+	        Job job1 = new Job();
+	        job1.setCompany("Company 1");
+	        job1.setDesignation("Designation");
+	        job1.setId(1);
+	        job1.setStartDate(LocalDate.of(2020, 1, 1));
+	        job1.setEndDate(LocalDate.of(2020, 3, 1));
+	        Job job2 = new Job();
+	        job2.setCompany("Company 2");
+	        job2.setDesignation("Designation");
+	        job2.setId(2);
+	        job2.setStartDate(LocalDate.of(2019, 5, 1));
+	        job2.setEndDate(LocalDate.of(2020, 1, 1));
+	        profile1.getJobs().clear();
+	        profile1.getJobs().add(job1);
+	        profile1.getJobs().add(job2);
+
+	        userProfileRepository.save(profile1);
+
+	        return "profile";
 	}
 	@GetMapping("/edit")
 	public String testAdmin() {
--- src/main/java/com/pravalika/projects/resumeportal/models/UserProfile.java
+++ src/main/java/com/pravalika/projects/resumeportal/models/UserProfile.java
@@ -1,9 +1,16 @@
 package com.pravalika.projects.resumeportal.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +29,22 @@ public class UserProfile {
 	private String phone;
 	private String designation;
 	
+	@Override
+	public String toString() {
+		return "UserProfile [id=" + id + ", theme=" + theme + ", summary=" + summary + ", userName=" + userName
+				+ ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone
+				+ ", designation=" + designation + ", jobs=" + jobs + "]";
+	}
+	@OneToMany(cascade = CascadeType.ALL,orphanRemoval=true)
+	@JoinColumn(name="job_id")
+	private List<Job> jobs=new ArrayList<Job>();
+	
+	public List<Job> getJobs() {
+		return jobs;
+	}
+	public void setJobs(List<Job> jobs) {
+		this.jobs = jobs;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
